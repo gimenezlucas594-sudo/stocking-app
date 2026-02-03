c33baefa179b8489af0101899a61ea09ca466c64
gimenezlucas594-sudo <gimenezlucas594@gmail.com>
2026-02-02 22:42:12 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -806,6 +806,7 @@ const abrirPago = () => {
                     </div>
                 </div>
             </div>
+        
             {/* Modal de Medios de Pago */}
             {mostrarPago && (
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
